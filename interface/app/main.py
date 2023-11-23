bb74f466d767513be99e45ee09e15f374062ea73
SerialCookie <cookieserial@gmail.com>
2023-11-23 13:55:08 +0100

    bootstrap

--- interface/app/main.py
+++ interface/app/main.py
@@ -5,4 +5,12 @@ app.config['SECRET_KEY'] = 'J$n@M,D(25)'
 
 @app.route('/')
 def index():
-    return render_template('index.hmtl')
+    return render_template('index.html')
+
+@app.route('game')
+def game():
+    return render_template('game.html')
+
+@app.route('ai')
+def ai():
+    return render_template('ai.html')
