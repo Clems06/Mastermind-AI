74e00e6b4d5eadd49f683635c4d1f922959340c8
SerialCookie <cookieserial@gmail.com>
2023-11-23 21:07:59 +0100

    dev

--- interface/app/main.py
+++ interface/app/main.py
@@ -3,13 +3,15 @@ from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'J$n@M,D(25)'
 
+colors={'red':"#D90404", 'blue':"#05C7F2", 'green':"#078C03", 'yellow':"#F2B705", 'orange':"#F25C05", 'pink':"#F288A4", 'violet':"#4A2ABF",'grey':"#606B73"}
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/game')
 def game():
-    return render_template('game.html')
+    return render_template('game.html', **colors)
 
 @app.route('/ai')
 def ai():
---
+++ interface/app/static/main.js
@@ -0,0 +1,23 @@
+var pin1 = document.getElementById('pin1');
+pin1.onchange = (event) => {
+    var inputText = event.target.value;
+    pin1.style.backgroundColor = inputText;
+}
+
+var pin2 = document.getElementById('pin1');
+pin1.onchange = (event) => {
+    var inputText = event.target.value;
+    pin1.style.backgroundColor = inputText;
+}
+
+var pin3 = document.getElementById('pin1');
+pin1.onchange = (event) => {
+    var inputText = event.target.value;
+    pin1.style.backgroundColor = inputText;
+}
+
+var pin4 = document.getElementById('pin1');
+pin1.onchange = (event) => {
+    var inputText = event.target.value;
+    pin1.style.backgroundColor = inputText;
+}
