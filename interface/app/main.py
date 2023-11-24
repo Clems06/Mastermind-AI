51da286db4810ae79ac6a9b0428479c225993a93
SerialCookie <cookieserial@gmail.com>
2023-11-24 21:14:24 +0100

    dev

--- interface/app/main.py
+++ interface/app/main.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, request
-import sys
-sys.path.append('...')
+from flask import Flask, render_template, request, session
+from flask_session import Session
+import flask_sijax
 from environment import *
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'J$n@M,D(25)'
-
-board=Board("Game")
+app.config['SESSION_TYPE']='filesystem'
+app.secret_key = 'J$n@M,D(25)'
+Session(app)
 
 @app.route('/')
 def index():
@@ -15,12 +15,14 @@ def index():
 @app.route('/game', methods=['POST', 'GET'])
 def game():
     if request.method=="GET":
-        board=Board("Game")
+        session['board']=Board("Game")
     else:
         pin1, pin2, pin3, pin4=request.form.get("pin1"), request.form.get("pin2"), request.form.get("pin3"), request.form.get("pin4")
         combination=todict(tuple([color_to_number[c] for c in (pin1, pin2, pin3, pin4)]))
         print(combination)
-    return render_template('game.html', **colors, board=board, tries=str(1))
+        session['board'].append(combination)
+        print(session['board'].lines)
+    return render_template('game.html', **colors, board=session['board'])
 
 @app.route('/ai')
 def ai():
